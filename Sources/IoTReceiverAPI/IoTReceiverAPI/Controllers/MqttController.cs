79113758a8cfb8b8f6cd60bb5672c90ba09f265c
monojit18 <datta.monojit@gmail.com>
2020-05-05 08:05:06 +0530

    Adding IoTMqtt client API

---
+++ Sources/IoTReceiverAPI/IoTReceiverAPI/Controllers/MqttController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using IoTReceiverAPI.Models;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Azure.Devices.Client;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IoTReceiverAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MqttController : Controller
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public string GetValues(string id)
+        {
+            return id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("api")]
+        public void Post([FromBody] MqttMessage mqttMessage)
+        {
+
+            StringValues connString;
+            var mqttMessageString = JsonConvert.SerializeObject(mqttMessage);
+            Request.Headers.TryGetValue("conn", out connString);
+            Console.WriteLine(connString.ToString());
+            Console.WriteLine(mqttMessageString);
+
+            Task.Run(async () =>
+            {
+
+                var dc = DeviceClient.CreateFromConnectionString(connString, TransportType.Mqtt);
+                var msg = new Message(Encoding.UTF8.GetBytes(mqttMessageString));
+                await dc.SendEventAsync(msg);
+                Console.WriteLine("Sent");
+
+            });
+
+        }
+
+    }
+}
---
+++ Sources/IoTReceiverAPI/IoTReceiverAPI/Models/MqttMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IoTReceiverAPI.Models
+{
+    public class MqttMessage
+    {
+
+        public string Data { get; set; }
+
+    }
+}
