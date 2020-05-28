811eaa651e1d9499e0e45bfcda4438c90f89bd81
monojit18 <datta.monojit@gmail.com>
2020-05-28 22:37:15 +0530

    Update aksauto-secret.ps1

--- Deployments/Setup/aksauto-secret.ps1
+++ Deployments/Setup/aksauto-secret.ps1
@@ -8,8 +8,8 @@ param([Parameter(Mandatory=$false)] [string] $dockerSecretName,
         [Parameter(Mandatory=$false)] [string] $mongopassword,
         [Parameter(Mandatory=$false)] [string] $namespaceName)
 
-# $dockerSecretCommand = "kubectl create secret docker-registry $dockerSecretName --docker-server=$dockerServer --docker-username=$dockerUsername --docker-password=$dockerPassword -n $namespaceName"
-# Invoke-Expression -Command $dockerSecretCommand
+$dockerSecretCommand = "kubectl create secret docker-registry $dockerSecretName --docker-server=$dockerServer --docker-username=$dockerUsername --docker-password=$dockerPassword -n $namespaceName"
+Invoke-Expression -Command $dockerSecretCommand
 
-# $mongoSecretCommand = "kubectl create secret generic $mongoSecretName --from-literal=mongohost=$mongohost --from-literal=mongouser=$mongouser --from-literal=mongopassword=$mongopassword -n $namespaceName"
-# Invoke-Expression -Command $mongoSecretCommand
+$mongoSecretCommand = "kubectl create secret generic $mongoSecretName --from-literal=mongohost=$mongohost --from-literal=mongouser=$mongouser --from-literal=mongopassword=$mongopassword -n $namespaceName"
+Invoke-Expression -Command $mongoSecretCommand
