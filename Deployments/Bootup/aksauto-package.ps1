4c1f17f64df58eeee472fefd4625a66895181d92
monojit18 <datta.monojit@gmail.com>
2020-05-30 09:36:47 +0530

    Delete aksauto-package.ps1

--- Deployments/Bootup/aksauto-package.ps1
+++
@@ -1,7 +0,0 @@
-param([Parameter(Mandatory=$false)] [string] $packageFilePath)
-
-$packageInfo = Get-Content $packageFilePath | ConvertFrom-Json
-$packageVersion = $packageInfo.version
-Write-Host $packageVersion
-
-Write-Host "##vso[task.setvariable variable=pkgVersion;]$packageVersion"
