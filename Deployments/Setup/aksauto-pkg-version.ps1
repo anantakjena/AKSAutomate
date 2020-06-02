8a49adbd650a810b211db3f072833cff674508da
monojit18 <datta.monojit@gmail.com>
2020-06-02 22:00:40 +0530

    Update aksauto-pkg-version.ps1

--- Deployments/Setup/aksauto-pkg-version.ps1
+++ Deployments/Setup/aksauto-pkg-version.ps1
@@ -4,4 +4,6 @@ param([Parameter(Mandatory=$false)] [string] $inVersionToken,
 
 $settingsInfo = Get-Content -Path $packageFilePath -Raw | ConvertFrom-Json
 $versionInfo = $settingsInfo.$inVersionToken
+Write-Host $settingsInfo
+Write-Host $versionInfo
 Write-Host "##vso[task.setvariable variable=$outVersionToken]$versionInfo"
