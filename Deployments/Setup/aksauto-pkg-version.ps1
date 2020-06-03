1137e986a2e5a5c76423746f291105311c2f63a8
monojit18 <datta.monojit@gmail.com>
2020-06-03 07:35:17 +0530

    Delete aksauto-pkg-version.ps1

--- Deployments/Setup/aksauto-pkg-version.ps1
+++
@@ -1,9 +0,0 @@
-param([Parameter(Mandatory=$false)] [string] $inVersionToken,
-        [Parameter(Mandatory=$false)] [string] $outVersionToken,
-        [Parameter(Mandatory=$false)] [string] $packageFilePath)
-
-$settingsInfo = Get-Content -Path $packageFilePath -Raw | ConvertFrom-Json
-$versionInfo = $settingsInfo.$inVersionToken
-Write-Host $settingsInfo
-Write-Host $versionInfo
-Write-Host "##vso[task.setvariable variable=$outVersionToken]$versionInfo"
