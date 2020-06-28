c376740dc515ed043beedfa8d9c11d900e65684b
Monojit Datta <datta.monojit@gmail.com>
2020-06-28 17:38:50 +0530

    Update aksauto-apiserver-ip-config.ps1

--- Deployments/Setup/DEV/Security/aksauto-apiserver-ip-config.ps1
+++ Deployments/Setup/DEV/Security/aksauto-apiserver-ip-config.ps1
@@ -7,7 +7,7 @@ $aksUpdateCommand = "az aks update -g $resourceGroup -n $clusterName --api-serve
 
 if ($shouldEnable -eq $false)
 {
-    $aksUpdateCommand = $aksUpdateCommand -join ""
+    $aksUpdateCommand = $aksUpdateCommand -join "''"
 }
 else
 {
